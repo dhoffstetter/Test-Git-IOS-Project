db2047a25799f0904ec9c8e69f17c384e37873e7
dhoffstetter <dhoffstetter@mac.com>
2014-04-09 13:26:03 -0700

    added a test class

---
+++ GitTest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by Diane Hoffstetter on 4/9/14.
+//  Copyright (c) 2014 Diane Hoffstetter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ GitTest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by Diane Hoffstetter on 4/9/14.
+//  Copyright (c) 2014 Diane Hoffstetter. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
