4a88b8d4f506948cfc567bfd2456d0149753bb25
Diane Hoffstetter <Diane@Diane-Hoffstetters-iMac.local>
2014-04-09 13:03:39 -0700

    Initial Commit

---
+++ GitTest/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  GitTest
+//
+//  Created by Diane Hoffstetter on 4/9/14.
+//  Copyright (c) 2014 Diane Hoffstetter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  GitTest
+//
+//  Created by Diane Hoffstetter on 4/9/14.
+//  Copyright (c) 2014 Diane Hoffstetter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
